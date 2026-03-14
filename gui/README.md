a5c44269b36b19feb8d55a8b0b6cedc32915deba
DerPenz <mike2407@t-online.de>
2026-03-14 14:10:30 +0100

    restructure to use uv workspaces

---
+++ gui/README.md
@@ -0,0 +1,3 @@
+## Scored GUI
+
+a GUI to use trained models and test them as well as recording some data
---
+++ scored-lib/src/scored_lib/__init__.py
@@ -0,0 +1,5 @@
+def test():
+    """
+    A simple test function that prints a message.
+    """
+    print("Hello, World!")
